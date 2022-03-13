8e06008ea94b3e972df395b760e337d3c937afb0
Md Moin <developermdmoin@gmail.com>
2022-03-13 04:28:50 -0800

    Moin

--- unit-5/sprint-4/evaluation/C4/src/config/db.js
+++
@@ -1,9 +0,0 @@
-
-  const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect("mongodb://localhost:27017/c4") // this for local host 
-};
-
-
-//  return "mongodb+srv://moinjack788:moin_788@cluster0.lv7ud.mongodb.net/web20?retryWrites=true&w=majority" this for atlas
